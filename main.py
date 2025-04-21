ac900fd329cd9ae7cdd1d103f77741ec0333629d
Michal Kubaščík <michal.kubascik26799@gmail.com>
2025-04-21 21:34:11 +0200

    added acount ID
    added account id to config.json, such as personal number, etc

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ with open("configs/credentials.json",'r') as fd:
      credentials_items = json.load(fd)
      print(credentials_items)
 
-dexcom_users=[]
-libre_users=[]
+dexcom_users={}
+libre_users={}
 for credentials in credentials_items:
     print(credentials)
     if(credentials['type']=="Dexcom"):
-        dexcom_users.append(Dexcom(
+        dexcom_users.update({credentials['user_id'] : Dexcom(
             username=credentials['username'], 
             password=credentials['password'], 
-            region=credentials['region']))
+            region=credentials['region'])})
     elif(credentials['type'] == "LibreLinkUp"):
-        libre_users.append(LibreLinkUpClient(
+        libre_users.update({credentials['user_id'] :LibreLinkUpClient(
         username=credentials['username'],
         password=credentials['password'],
         url=credentials['url'],
         version="4.14.0",
-        ))
+        )})
     else:
         print(f"Wrong type %s"%(credentials["type"]))
 
-glucose_reading = dexcom_users[0].get_latest_glucose_reading()
-print(glucose_reading)
+for dexcom_key in dexcom_users:
+    print(f"id: %s, obj: %s"%(dexcom_key, dexcom_users[dexcom_key]))
+    glucose_reading = dexcom_users[dexcom_key]._username
+    print(glucose_reading)
 
-for client in libre_users:
-    client.login()
-
-glucose_data = libre_users[0].get_raw_connection()
-print(glucose_data['glucoseMeasurement']['Value'])
+for libre_key in libre_users:
+    print(f"id: %s, obj: %s"%(libre_key, libre_users[libre_key]))
+    libre_users[libre_key].login()
+    glucose_data = libre_users[libre_key].get_raw_connection()
+    print(glucose_data['glucoseMeasurement']['Value'])
